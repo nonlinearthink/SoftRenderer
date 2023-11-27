eaa68f634d0c6c286ab7533f43317596d48b4e94
nonlinearthink <2832203391@qq.com>
2023-11-27 17:59:15 +0800

    temp

--- include/core/vec.h
+++ include/core/vec.h
@@ -1,73 +1,35 @@
-template <typename T, size_t N>
-class BaseVectorN {
-   public:
-    BaseVectorN<T, N>();
-    ~BaseVectorN<T, N>();
-
-    T &operator[](size_t i) const;
-    BaseVectorN<T, N> &operator-() const;
-    BaseVectorN<T, N> operator+(const T &rhs) const;
-    BaseVectorN<T, N> operator-(const T &rhs) const;
-    BaseVectorN<T, N> operator*(const T &rhs) const;
-    BaseVectorN<T, N> operator/(const T &rhs) const;
-    BaseVectorN<T, N> operator+(const BaseVectorN<T, N> &rhs) const;
-    BaseVectorN<T, N> operator-(const BaseVectorN<T, N> &rhs) const;
-    BaseVectorN<T, N> operator*(const BaseVectorN<T, N> &rhs) const;
-    BaseVectorN<T, N> operator/(const BaseVectorN<T, N> &rhs) const;
-    BaseVectorN<T, N> &operator+=(const BaseVectorN<T, N> &rhs);
-    BaseVectorN<T, N> &operator-=(const BaseVectorN<T, N> &rhs);
-    BaseVectorN<T, N> &operator*=(const BaseVectorN<T, N> &rhs);
-    BaseVectorN<T, N> &operator/=(const BaseVectorN<T, N> &rhs);
-
-   protected:
-    T _v[N]{};
-};
+#pragma once
 
 template <typename T>
-class Vector2 : protected BaseVectorN<T, 2> {
+class Vector2 {
    public:
-    Vector2<T>() : BaseVectorN<T, 2>(){};
-    Vector2<T>(T x, T y);
+    T x, y;
+    Vector2<T>() : x(), y(){};
+    Vector2<T>(T _x, T _y) : x(_x), y(_y) {}
 
-    T &x() const;
-    T &y() const;
-    void setX(T x);
-    void setY(T y);
+    inline Vector2<T> operator+(const Vector2<T>& rhs) const {
+        return Vector2(x + rhs.x, y + rhs.y);
+    };
+    inline Vector2<T> operator-(const Vector2<T>& rhs) const {
+        return Vector2(x - rhs.x, y - rhs.y);
+    };
+    inline Vector2<T> operator*(const Vector2<T>& rhs) const {
+        return Vector2(x * rhs.x, y * rhs.y);
+    };
+    inline Vector2<T> operator/(const Vector2<T>& rhs) const {
+        return Vector2(x / rhs.x, y / rhs.y);
+    };
 };
 
-template <typename T>
-class Vector3 : protected BaseVectorN<T, 3> {
-   public:
-    Vector3<T>() : BaseVectorN<T, 3>(){};
-    Vector3<T>(T x, T y, T z);
-
-    T &x() const;
-    T &y() const;
-    T &z() const;
-    void setX(T x);
-    void setY(T y);
-    void setZ(T z);
-};
-
-template <typename T>
-class Vector4 : protected BaseVectorN<T, 4> {
-   public:
-    Vector4<T>() : BaseVectorN<T, 4>(){};
-    Vector4<T>(T x, T y, T z, T w);
-
-    T &x() const;
-    T &y() const;
-    T &z() const;
-    T &w() const;
-    void setX(T x);
-    void setY(T y);
-    void setZ(T z);
-    void setW(T w);
-};
+// template <typename T>
+// class Vector3 {
+//    public:
+//     T x, y, z;
+//     Vector3<T>();
+//     Vector3<T>(T x, T y, T z);
+// };
 
 using Vector2f = Vector2<float>;
 using Vector2i = Vector2<int>;
-using Vector3f = Vector3<float>;
-using Vector3i = Vector3<int>;
-using Vector4f = Vector4<float>;
-using Vector4i = Vector4<int>;
+// using Vector3f = Vector3<float>;
+// using Vector3i = Vector3<int>;
--- src/core/vec.cpp
+++ src/core/vec.cpp
@@ -1,244 +1,246 @@
 #include "core/vec.h"
 
-template <typename T, size_t N>
-BaseVectorN<T, N>::BaseVectorN() {
-    _v = new T[N];
-}
+// #include "core/vec.h"
 
-template <typename T, size_t N>
-BaseVectorN<T, N>::~BaseVectorN() {
-    delete[] _v;
-}
+// template <typename T, size_t N>
+// BaseVectorN<T, N>::BaseVectorN() {
+//     _v = new T[N];
+// }
 
-template <typename T, size_t N>
-T &BaseVectorN<T, N>::operator[](size_t i) const {
-    return _v[i];
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N>::~BaseVectorN() {
+//     delete[] _v;
+// }
 
-template <typename T, size_t N>
-BaseVectorN<T, N> &BaseVectorN<T, N>::operator-() const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = -_v[i];
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// T &BaseVectorN<T, N>::operator[](size_t i) const {
+//     return _v[i];
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator+(const T &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] + rhs;
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> &BaseVectorN<T, N>::operator-() const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = -_v[i];
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator-(const T &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] - rhs;
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator+(const T &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] + rhs;
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator*(const T &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] * rhs;
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator-(const T &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] - rhs;
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator/(const T &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] / rhs;
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator*(const T &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] * rhs;
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator+(
-    const BaseVectorN<T, N> &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] + rhs._v[i];
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator/(const T &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] / rhs;
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator-(
-    const BaseVectorN<T, N> &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] - rhs._v[i];
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator+(
+//     const BaseVectorN<T, N> &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] + rhs._v[i];
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator*(
-    const BaseVectorN<T, N> &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] * rhs._v[i];
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator-(
+//     const BaseVectorN<T, N> &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] - rhs._v[i];
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> BaseVectorN<T, N>::operator/(
-    const BaseVectorN<T, N> &rhs) const {
-    BaseVectorN<T, N> &newV{};
-    for (size_t i = 0; i < N; i++) {
-        newV._v[i] = _v[i] / rhs._v[i];
-    }
-    return newV;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator*(
+//     const BaseVectorN<T, N> &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] * rhs._v[i];
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> &BaseVectorN<T, N>::operator+=(const BaseVectorN<T, N> &rhs) {
-    for (size_t i = 0; i < N; i++) {
-        _v[i] += rhs._v[i];
-    }
-    return this;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> BaseVectorN<T, N>::operator/(
+//     const BaseVectorN<T, N> &rhs) const {
+//     BaseVectorN<T, N> &newV{};
+//     for (size_t i = 0; i < N; i++) {
+//         newV._v[i] = _v[i] / rhs._v[i];
+//     }
+//     return newV;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> &BaseVectorN<T, N>::operator-=(const BaseVectorN<T, N> &rhs) {
-    for (size_t i = 0; i < N; i++) {
-        _v[i] -= rhs._v[i];
-    }
-    return this;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> &BaseVectorN<T, N>::operator+=(const BaseVectorN<T, N> &rhs) {
+//     for (size_t i = 0; i < N; i++) {
+//         _v[i] += rhs._v[i];
+//     }
+//     return this;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> &BaseVectorN<T, N>::operator*=(const BaseVectorN<T, N> &rhs) {
-    for (size_t i = 0; i < N; i++) {
-        _v[i] *= rhs._v[i];
-    }
-    return this;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> &BaseVectorN<T, N>::operator-=(const BaseVectorN<T, N> &rhs) {
+//     for (size_t i = 0; i < N; i++) {
+//         _v[i] -= rhs._v[i];
+//     }
+//     return this;
+// };
 
-template <typename T, size_t N>
-BaseVectorN<T, N> &BaseVectorN<T, N>::operator/=(const BaseVectorN<T, N> &rhs) {
-    for (size_t i = 0; i < N; i++) {
-        _v[i] /= rhs._v[i];
-    }
-    return this;
-};
+// template <typename T, size_t N>
+// BaseVectorN<T, N> &BaseVectorN<T, N>::operator*=(const BaseVectorN<T, N> &rhs) {
+//     for (size_t i = 0; i < N; i++) {
+//         _v[i] *= rhs._v[i];
+//     }
+//     return this;
+// };
 
-template <typename T>
-Vector2<T>::Vector2(T x, T y) : BaseVectorN<T, 2>() {
-    this->_v[0] = x;
-    this->_v[1] = y;
-}
+// template <typename T, size_t N>
+// BaseVectorN<T, N> &BaseVectorN<T, N>::operator/=(const BaseVectorN<T, N> &rhs) {
+//     for (size_t i = 0; i < N; i++) {
+//         _v[i] /= rhs._v[i];
+//     }
+//     return this;
+// };
 
-template <typename T>
-T &Vector2<T>::x() const {
-    return this->_v[0];
-}
+// template <typename T>
+// Vector2<T>::Vector2(T x, T y) : BaseVectorN<T, 2>() {
+//     this->_v[0] = x;
+//     this->_v[1] = y;
+// }
 
-template <typename T>
-T &Vector2<T>::y() const {
-    return this->_v[1];
-}
+// template <typename T>
+// T &Vector2<T>::x() const {
+//     return this->_v[0];
+// }
 
-template <typename T>
-void Vector2<T>::setX(T x) {
-    this->_v[0] = x;
-}
+// template <typename T>
+// T &Vector2<T>::y() const {
+//     return this->_v[1];
+// }
 
-template <typename T>
-void Vector2<T>::setY(T y) {
-    this->_v[1] = y;
-}
+// template <typename T>
+// void Vector2<T>::setX(T x) {
+//     this->_v[0] = x;
+// }
 
-template <typename T>
-Vector3<T>::Vector3(T x, T y, T z) : BaseVectorN<T, 3>() {
-    this->_v[0] = x;
-    this->_v[1] = y;
-    this->_v[2] = z;
-}
+// template <typename T>
+// void Vector2<T>::setY(T y) {
+//     this->_v[1] = y;
+// }
 
-template <typename T>
-T &Vector3<T>::x() const {
-    return this->_v[0];
-}
+// template <typename T>
+// Vector3<T>::Vector3(T x, T y, T z) : BaseVectorN<T, 3>() {
+//     this->_v[0] = x;
+//     this->_v[1] = y;
+//     this->_v[2] = z;
+// }
 
-template <typename T>
-T &Vector3<T>::y() const {
-    return this->_v[1];
-}
+// template <typename T>
+// T &Vector3<T>::x() const {
+//     return this->_v[0];
+// }
 
-template <typename T>
-T &Vector3<T>::z() const {
-    return this->_v[2];
-}
+// template <typename T>
+// T &Vector3<T>::y() const {
+//     return this->_v[1];
+// }
 
-template <typename T>
-void Vector3<T>::setX(T x) {
-    this->_v[0] = x;
-}
+// template <typename T>
+// T &Vector3<T>::z() const {
+//     return this->_v[2];
+// }
 
-template <typename T>
-void Vector3<T>::setY(T y) {
-    this->_v[1] = y;
-}
+// template <typename T>
+// void Vector3<T>::setX(T x) {
+//     this->_v[0] = x;
+// }
 
-template <typename T>
-void Vector3<T>::setZ(T z) {
-    this->_v[2] = z;
-}
+// template <typename T>
+// void Vector3<T>::setY(T y) {
+//     this->_v[1] = y;
+// }
 
-template <typename T>
-Vector4<T>::Vector4(T x, T y, T z, T w) : BaseVectorN<T, 4>() {
-    this->_v[0] = x;
-    this->_v[1] = y;
-    this->_v[2] = z;
-    this->_v[3] = w;
-}
+// template <typename T>
+// void Vector3<T>::setZ(T z) {
+//     this->_v[2] = z;
+// }
 
-template <typename T>
-T &Vector4<T>::x() const {
-    return this->_v[0];
-}
+// template <typename T>
+// Vector4<T>::Vector4(T x, T y, T z, T w) : BaseVectorN<T, 4>() {
+//     this->_v[0] = x;
+//     this->_v[1] = y;
+//     this->_v[2] = z;
+//     this->_v[3] = w;
+// }
 
-template <typename T>
-T &Vector4<T>::y() const {
-    return this->_v[1];
-}
+// template <typename T>
+// T &Vector4<T>::x() const {
+//     return this->_v[0];
+// }
 
-template <typename T>
-T &Vector4<T>::z() const {
-    return this->_v[2];
-}
+// template <typename T>
+// T &Vector4<T>::y() const {
+//     return this->_v[1];
+// }
 
-template <typename T>
-T &Vector4<T>::w() const {
-    return this->_v[3];
-}
+// template <typename T>
+// T &Vector4<T>::z() const {
+//     return this->_v[2];
+// }
 
-template <typename T>
-void Vector4<T>::setX(T x) {
-    this->_v[0] = x;
-}
+// template <typename T>
+// T &Vector4<T>::w() const {
+//     return this->_v[3];
+// }
 
-template <typename T>
-void Vector4<T>::setY(T y) {
-    this->_v[1] = y;
-}
+// template <typename T>
+// void Vector4<T>::setX(T x) {
+//     this->_v[0] = x;
+// }
 
-template <typename T>
-void Vector4<T>::setZ(T z) {
-    this->_v[2] = z;
-}
+// template <typename T>
+// void Vector4<T>::setY(T y) {
+//     this->_v[1] = y;
+// }
 
-template <typename T>
-void Vector4<T>::setW(T w) {
-    this->_v[3] = w;
-}
+// template <typename T>
+// void Vector4<T>::setZ(T z) {
+//     this->_v[2] = z;
+// }
+
+// template <typename T>
+// void Vector4<T>::setW(T w) {
+//     this->_v[3] = w;
+// }
