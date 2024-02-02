56674e2c437e67c6188c257524afdc7effbdd6fa
nonlinearthink <2832203391@qq.com>
2024-02-02 13:16:20 +0800

    camera

--- SoftRenderer/include/SoftRenderer/camera.h
+++ SoftRenderer/include/SoftRenderer/camera.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "SoftRenderer/camera.h"
 #include "SoftRenderer/transform.h"
 #include "SoftRenderer/vec.h"
 
 namespace SoftRenderer {
-constexpr float DEFAULT_FOV = 30.f;
+enum class ProjectionMode { PERSPECTIVE, ORTHOGRAPHIC };
+
+constexpr float DEFAULT_FOV = 30;
 constexpr float DEFAULT_ASPECT = 16.f / 9.f;
+constexpr float DEFAULT_NEAR_Z = -1;
+constexpr float DEFAULT_FAR_Z = -1000;
+constexpr ProjectionMode DEFAULT_PROJECTION_MODE = ProjectionMode::PERSPECTIVE;
 
 // Camera Class
 class Camera {
@@ -17,7 +23,10 @@ public:
     inline void set_target(const Vector3f& target);
     inline void set_up(const Vector3f& up);
     inline void set_fov(float fov);
-    inline void set_aspect(float aspect_);
+    inline void set_aspect(float aspect);
+    inline void set_near_z(float near_z);
+    inline void set_far_z(float far_z);
+    inline void set_projection_mode(ProjectionMode projection_mode);
 
     // Updates per frame to compute the view matrix and projection
     // matrix of this camera.
@@ -27,44 +36,62 @@ private:
     Vector3f position_;
     Vector3f target_;
     Vector3f up_;
+
     float fov_{DEFAULT_FOV};
     float aspect_{DEFAULT_ASPECT};
-    Transform view_matrix_;
-    bool is_view_matrix_dirty_{false};
-    Transform projection_matrix_;
-    bool is_projection_matrix_dirty_{false};
+    float near_z_{DEFAULT_FAR_Z};
+    float far_z_{DEFAULT_FAR_Z};
+    ProjectionMode projection_mode_{DEFAULT_PROJECTION_MODE};
 
-    void ComputeViewMatrix();
-    void ComputeProjectionMatrix();
+    Transform view_transform_;
+    bool is_view_transform_dirty_{false};
+    Transform projection_transform_;
+    bool is_projection_transform_dirty_{false};
+
+    void ComputeViewTransform();
+    void ComputeProjectionTransform();
 };
 
 #pragma region Camera Inline Implementation
-
 inline void Camera::set_position(const Vector3f& position) {
-    is_view_matrix_dirty_ = true;
+    is_view_transform_dirty_ = true;
     position_.CopyFrom(position);
 }
 
 inline void Camera::set_target(const Vector3f& target) {
-    is_view_matrix_dirty_ = true;
+    is_view_transform_dirty_ = true;
     target_.CopyFrom(target);
 }
 
 inline void Camera::set_up(const Vector3f& up) {
-    is_view_matrix_dirty_ = true;
+    is_view_transform_dirty_ = true;
     up_.CopyFrom(up);
 }
 
 inline void Camera::set_fov(float fov) {
-    is_projection_matrix_dirty_ = true;
+    is_projection_transform_dirty_ = true;
     fov_ = fov;
 }
 
 inline void Camera::set_aspect(float aspect) {
-    is_projection_matrix_dirty_ = true;
+    is_projection_transform_dirty_ = true;
     aspect_ = aspect;
 }
 
+inline void Camera::set_near_z(float near_z) {
+    is_projection_transform_dirty_ = true;
+    near_z_ = near_z;
+}
+
+inline void Camera::set_far_z(float far_z) {
+    is_projection_transform_dirty_ = true;
+    far_z_ = far_z;
+}
+
+inline void Camera::set_projection_mode(ProjectionMode projection_mode) {
+    is_projection_transform_dirty_ = true;
+    projection_mode_ = projection_mode;
+}
 #pragma endregion Camera Inline Implementation
 
 class CameraBuilder {
@@ -74,18 +101,24 @@ public:
     inline CameraBuilder& SetUp(const Vector3f& up);
     inline CameraBuilder& SetFov(float fov);
     inline CameraBuilder& SetAspect(float aspect);
-    inline std::unique_ptr<Camera> Build();
+    inline CameraBuilder& SetNearZ(float near_z);
+    inline CameraBuilder& SetFarZ(float far_z);
+    inline CameraBuilder& SetProjectionMode(ProjectionMode projection_mode);
+    [[nodiscard]] std::unique_ptr<Camera> Build() const;
 
 private:
     Vector3f position_;
     Vector3f target_;
     Vector3f up_;
+
     float fov_{DEFAULT_FOV};
     float aspect_{DEFAULT_ASPECT};
+    float near_z_{DEFAULT_FAR_Z};
+    float far_z_{DEFAULT_FAR_Z};
+    ProjectionMode projection_mode_{DEFAULT_PROJECTION_MODE};
 };
 
 #pragma region CameraBuilder Inline Implementation
-
 inline CameraBuilder& CameraBuilder::SetPosition(const Vector3f& position) {
     position_.CopyFrom(position);
     return *this;
@@ -111,13 +144,20 @@ inline CameraBuilder& CameraBuilder::SetAspect(float aspect) {
     return *this;
 }
 
-inline std::unique_ptr<Camera> CameraBuilder::Build() {
-    auto camera = std::make_unique<Camera>(position_, target_, up_);
-    camera->set_fov(fov_);
-    camera->set_aspect(aspect_);
-
-    return camera;
+inline CameraBuilder& CameraBuilder::SetNearZ(float near_z) {
+    near_z_ = near_z;
+    return *this;
 }
 
+inline CameraBuilder& CameraBuilder::SetFarZ(float far_z) {
+    far_z_ = far_z;
+    return *this;
+}
+
+inline CameraBuilder& CameraBuilder::SetProjectionMode(
+    ProjectionMode projection_mode) {
+    projection_mode_ = projection_mode;
+    return *this;
+}
 #pragma endregion CameraBuilder Inline Implementation
 }  // namespace SoftRenderer
--- SoftRenderer/src/camera.cpp
+++ SoftRenderer/src/camera.cpp
@@ -1,17 +1,46 @@
 #include "SoftRenderer/camera.h"
 
+#include <cmath>
+
+#include "SoftRenderer/transform.h"
+
 using namespace SoftRenderer;
 
-void Camera::ComputeViewMatrix() {}
+void Camera::ComputeViewTransform() {
+    if (!is_view_transform_dirty_) {
+        view_transform_ =
+            Transform::LookAt(position_, target_ - position_, up_);
+    }
+}
 
-void Camera::ComputeProjectionMatrix() {}
+void Camera::ComputeProjectionTransform() {
+    if (is_projection_transform_dirty_) {
+        float bottom = -near_z_ * std::tan(fov_ / 2);
+        float top = near_z_ * std::tan(fov_ / 2);
+        float left = -top * aspect_;
+        float right = top * aspect_;
+        if (projection_mode_ == ProjectionMode::PERSPECTIVE) {
+            projection_transform_ = Transform::Perspective(
+                left, right, bottom, top, far_z_, near_z_);
+        } else {
+            projection_transform_ = Transform::Orthographic(
+                left, right, bottom, top, far_z_, near_z_);
+        }
+    }
+}
 
 void Camera::Update() {
-    if (!is_view_matrix_dirty_) {
-        ComputeViewMatrix();
-    }
+    ComputeViewTransform();
+    ComputeProjectionTransform();
+}
 
-    if (is_projection_matrix_dirty_) {
-        ComputeProjectionMatrix();
-    }
+std::unique_ptr<Camera> CameraBuilder::Build() const {
+    auto camera = std::make_unique<Camera>(position_, target_, up_);
+    camera->set_fov(fov_);
+    camera->set_aspect(aspect_);
+    camera->set_near_z(near_z_);
+    camera->set_far_z(far_z_);
+    camera->set_projection_mode(projection_mode_);
+
+    return camera;
 }
